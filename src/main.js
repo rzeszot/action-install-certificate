d95720c5ccf486b9b9f6c14c0f67f94a62c06550
Damian Rzeszot <damian.rzeszot@gmail.com>
2020-05-10 01:14:40 +0200

    Rebuild

--- src/main.js
+++ src/main.js
@@ -1,16 +1,17 @@
 const core = require('@actions/core');
 const exec = require('@actions/exec');
 const tmp = require('tmp');
+const fs = require('fs-extra');
 
 
 async function keychain_import(p12_content, p12_password, keychain_name) {
   const buffer = Buffer.from(p12_content, 'base64')
-  const tempFile = tmp.fileSync()
-  fs.writeFileSync(tempFile.name, buffer)
+  const tempFile = tmp.fileSync().name
+  fs.writeFileSync(tempFile, buffer)
 
-  await exec.exec('security', 'import', tempFile.name, '-t', 'agg', '-k', keychain_name, '-P', `'${p12_password}'`, '-a');
+  await exec.exec('security', ['import', tempFile, '-t', 'agg', '-k', keychain_name, '-P', p12_password, '-A']);
 
-  fs.fs.unlinkSync(tempFile.name)
+  fs.unlinkSync(tempFile.name)
 }
 
 
@@ -21,23 +22,20 @@ async function run() {
 
     const p12content = core.getInput('p12-base64')
     const p12password = core.getInput('p12-password')
-
-    if (os.platform() !== 'darwin') {
-      throw 'Action requires macOS agent.';
-    }
-
-    //
-
     const keychain = "build.keychain";
 
     // Create keychain
 
-    await exec.exec('security', 'create-keychain', '-p', '""', keychain);
-    keychain_import(p12content, p12_password, keychain);
+    core.debug(`create-keychain`);
+    await exec.exec('security', ['create-keychain', '-p', '""', keychain]);
 
-    await exec.exec('security', 'default-keychain', '-s', keychain);
-    await exec.exec('security', 'unlock-keychain', '-p', '""', keychain);
-    await exec.exec('security', 'set-key-partition-list', '-S', 'apple-tool:,apple:,codesign:', '-s', '-k', '""', keychain);
+    core.debug(`import p12`);
+    keychain_import(p12content, p12password, keychain);
+
+    core.debug(`xxx`);
+    await exec.exec('security', ['default-keychain', '-s', keychain]);
+    await exec.exec('security', ['unlock-keychain', '-p', '""', keychain]);
+    await exec.exec('security', ['set-key-partition-list', '-S', 'apple-tool:,apple:,codesign:', '-s', '-k', '""', keychain]);
 
   } catch (error) {
     core.setFailed(error.message);
